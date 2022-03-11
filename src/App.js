2de52e285ef850c0ce3e1ad84819497652427198
Kevin <kevin.svanback@gmail.com>
2022-03-11 14:29:33 +0100

    clean up boiler code and added components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import 'firebase/analytics';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+// import { useAuthState } from 'react-firebase-hooks/auth';
 import './App.css';
+// import { useCollectionData } from 'react-firebase-hooks/firestore';
 
-function App() {
+firebase.initializeApp({
+  apiKey: "AIzaSyBi4o5_SIj2hsJc_XDJ_6NqhsAttCoxU9Q",
+  authDomain: "chat-app-1bd48.firebaseapp.com",
+  projectId: "chat-app-1bd48",
+  storageBucket: "chat-app-1bd48.appspot.com",
+  messagingSenderId: "391589411439",
+  appId: "1:391589411439:web:89dd2a2d3bb1d719efeb9a"
+});
+
+// const auth = firebase.auth();
+// const firestore = firebase.firestore();
+
+const App = () => {
+  // const [user] = useAuthState(auth);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+
       </header>
+
+      <section>
+        {/* {user ? <ChatRoom /> : <SignIn />} */}
+      </section>
     </div>
   );
-}
+};
 
 export default App;
